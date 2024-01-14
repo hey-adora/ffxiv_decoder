630f88bd8b1c34cae3b31c6d649e1f42bcaaa603
Your Name <you@example.com>
2024-01-14 19:05:26 +0200

    new broken decoder

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let mut buffer = reader::Buffer::from_file(FILE_PATH);
     let metadata = parser::sqex_scd::Metadata::new(&mut buffer);
     let decoded = decoder::sqex_scd::decode(metadata, &mut buffer);
+    println!("{}", decoded.len());
     for (index, byte) in decoded.iter().enumerate() {
         if index % 16 == 0 {
             print!("\x1b[39;49m\n")
@@ -19,6 +20,9 @@ fn main() {
             print!("\x1b[39;49m{:02x} ", byte)
         }
     }
+
+    std::fs::write("./audio", &decoded).unwrap();
+
     //visualizer::buffer::print(&buffer);
 }
 
