2019f401258caaf187c50b58e6289700197ee22c
Your Name <you@example.com>
2024-01-14 19:05:26 +0200

    stereo decoding added

--- src/decoder/sqex_scd.rs
+++ src/decoder/sqex_scd.rs
@@ -15,47 +15,111 @@ const MSADPCM_COEFS: [[i16; 2]; 7] = [
     [392, -232],
 ];
 
-fn add_u2_to_u8_vec(add: &mut Vec<u8>, with: [u8; 2]) {
-    for item in with {
-        add.push(item)
+struct BlockHeader {
+    delta: i128,
+    sample1: i128,
+    sample2: i128,
+    coeff1: i128,
+    coeff2: i128,
+}
+
+impl BlockHeader {
+    pub fn new(block: &[u8], current_channel: usize, total_channels: usize) -> BlockHeader {
+        let predictor_index = 0x00 + current_channel;
+        let mut predictor: i128 = block[predictor_index] as i128;
+
+        let initial_delta_index = total_channels + current_channel * 0x02;
+        let mut delta: i128 = i16::from_le_bytes([block[initial_delta_index], block[initial_delta_index + 0x01]]) as i128;
+
+        let sample1_index = total_channels + total_channels * 0x02 + current_channel * 0x02;
+        let mut sample1: i128 = i16::from_le_bytes([block[sample1_index], block[sample1_index + 0x01]]) as i128;
+        let sample2_index = sample1_index + total_channels * 0x02;
+        let mut sample2: i128 = i16::from_le_bytes([block[sample2_index], block[sample2_index + 0x01]]) as i128;
+
+
+        let coeff_index = (predictor & 0x07) as usize;
+        let coeff1: i128 = MSADPCM_COEFS[coeff_index][0] as i128;
+        let coeff2: i128 = MSADPCM_COEFS[coeff_index][1] as i128;
+
+        BlockHeader {
+            delta,
+            sample1,
+            sample2,
+            coeff1,
+            coeff2,
+        }
     }
 }
 
-pub fn decode_block(block: &[u8]) -> Vec<u8> {
+
+fn decode_byte(block_header: &mut BlockHeader, byte: u8, shift: usize) -> [u8; 2] {
+    let mut predictor = 0;
+    let nibble: i128 = match shift {
+        1 => NIBBLE_TO_INT[(byte & 0xf) as usize] as i128,
+        _ => NIBBLE_TO_INT[(byte >> 4) as usize] as i128,
+    };
+
+    predictor = ((block_header.sample1 * block_header.coeff1) + (block_header.sample2 * block_header.coeff2)) >> 8;
+    predictor = predictor + (nibble * block_header.delta);
+    predictor = clamp16(predictor);
+
+    block_header.sample2 = block_header.sample1;
+    block_header.sample1 = predictor;
+    block_header.delta = ((MSADPCM_STEPS[(nibble & 0xf) as usize] as i128) * block_header.delta) >> 8;
+    if block_header.delta < 16 { block_header.delta = 16 };
+
+    return (predictor as i16).to_ne_bytes();
+}
+
+
+fn decode_stereo_block(block: &[u8]) -> Vec<u8> {
     let mut temp_buffer: Vec<u8> = Vec::new();
-    let mut predictor: i32 = block[0x00] as i32;
-    let mut initial_delta: i32 = i16::from_le_bytes([block[0x01], block[0x02]]) as i32;
-    let mut sample1: i32 = i16::from_le_bytes([block[0x03], block[0x04]]) as i32;
-    let mut sample2: i32 = i16::from_le_bytes([block[0x05], block[0x06]]) as i32;
-    add_u2_to_u8_vec(&mut temp_buffer, (sample2 as i16).to_ne_bytes());
-    add_u2_to_u8_vec(&mut temp_buffer, (sample1 as i16).to_ne_bytes());
-    let coeff_index = (predictor & 0x07) as usize;
-    let coeff1: i32 = MSADPCM_COEFS[coeff_index][0] as i32;
-    let coeff2: i32 = MSADPCM_COEFS[coeff_index][1] as i32;
+
+    let mut left_channel_block_header: BlockHeader = BlockHeader::new(block, 0, 2);
+    let mut right_channel_block_header: BlockHeader = BlockHeader::new(block, 1, 2);
+
+    add_u2_to_u8_vec(&mut temp_buffer, (left_channel_block_header.sample2 as i16).to_ne_bytes());
+    add_u2_to_u8_vec(&mut temp_buffer, (right_channel_block_header.sample2 as i16).to_ne_bytes());
+
+    add_u2_to_u8_vec(&mut temp_buffer, (left_channel_block_header.sample1 as i16).to_ne_bytes());
+    add_u2_to_u8_vec(&mut temp_buffer, (right_channel_block_header.sample1 as i16).to_ne_bytes());
 
 
-    for index in 0x07..block.len() {
+    for index in (0x07 * 2)..block.len() {
         let byte = block[index];
+
+        let left_channel_decoded_block = decode_byte(&mut left_channel_block_header, byte, 0);
+        let right_channel_decoded_block = decode_byte(&mut right_channel_block_header, byte, 1);
+
+        add_u2_to_u8_vec(&mut temp_buffer, left_channel_decoded_block);
+        add_u2_to_u8_vec(&mut temp_buffer, right_channel_decoded_block);
+    }
+
+    temp_buffer
+}
+
+fn decode_mono_block(block: &[u8]) -> Vec<u8> {
+    let mut temp_buffer: Vec<u8> = Vec::new();
+
+    let mut mono_channel_block_header: BlockHeader = BlockHeader::new(block, 0, 1);
+
+    add_u2_to_u8_vec(&mut temp_buffer, (mono_channel_block_header.sample2 as i16).to_ne_bytes());
+    add_u2_to_u8_vec(&mut temp_buffer, (mono_channel_block_header.sample1 as i16).to_ne_bytes());
+
+
+    for index in (0x07 * 1)..block.len() {
+        let byte = block[index];
+
         for shift in 0..2 {
-            let nibble: i32 = match shift {
-                1 => NIBBLE_TO_INT[(byte & 0xf) as usize] as i32,
-                _ => NIBBLE_TO_INT[(byte >> 4) as usize] as i32,
-            };
-            predictor = ((sample1 * coeff1) + (sample2 * coeff2)) >> 8;
-            predictor = predictor + (nibble * initial_delta);
-            predictor = clamp16(predictor);
-            add_u2_to_u8_vec(&mut temp_buffer, (predictor as i16).to_ne_bytes());
-            sample2 = sample1;
-            sample1 = predictor;
-            initial_delta = ((MSADPCM_STEPS[(nibble & 0xf) as usize] as i32) * initial_delta) >> 8;
-            if initial_delta < 16 { initial_delta = 16 };
+            let mono_channel_decoded_block = decode_byte(&mut mono_channel_block_header, byte, shift);
+            add_u2_to_u8_vec(&mut temp_buffer, mono_channel_decoded_block);
         }
     }
 
     temp_buffer
 }
 
-pub fn decode(metadata: Metadata, buffer: &mut Buffer) -> Vec<u8> {
+pub fn decode(metadata: &Metadata, buffer: &mut Buffer) -> Vec<u8> {
     let offset: usize = metadata.audio_offset as usize;
     let frame_size: usize = metadata.entry_frame_size as usize;
     let mut output_buffer: Vec<u8> = Vec::new();
@@ -64,27 +128,52 @@ pub fn decode(metadata: Metadata, buffer: &mut Buffer) -> Vec<u8> {
     let length = metadata.entry_stream_size as usize;
 
 
-    while decoded < length {
-        let mut current_frame_size = frame_size;
-        if decoded + frame_size > length {
-            current_frame_size = length - decoded;
+    if metadata.entry_channels == 1 {
+        while decoded < length {
+            let mut current_frame_size = frame_size;
+            if decoded + frame_size > length {
+                current_frame_size = length - decoded;
+            }
+
+            let block: &[u8] = buffer.vec(offset + decoded, current_frame_size);
+            let mut decoded_block = decode_mono_block(block);
+            output_buffer.append(&mut decoded_block);
+
+            decoded += current_frame_size;
         }
+    } else if metadata.entry_channels == 2 {
+        while decoded < length {
+            let mut current_frame_size = frame_size;
+            if decoded + frame_size > length {
+                current_frame_size = length - decoded;
+            }
 
-        let block: &[u8] = buffer.vec(offset + decoded, current_frame_size);
-        let mut decoded_block = decode_block(block);
-        output_buffer.append(&mut decoded_block);
+            let block: &[u8] = buffer.vec(offset + decoded, current_frame_size);
+            let mut decoded_block = decode_stereo_block(block);
+            output_buffer.append(&mut decoded_block);
 
-        decoded += current_frame_size;
+            decoded += current_frame_size;
+        }
+    } else {
+        panic!("Unsupported channel count {}", metadata.entry_channels);
     }
+
 
     output_buffer
 }
 
-fn clamp16(val: i32) -> i32 {
+fn clamp16(val: i128) -> i128 {
     if val > 32767 {
         return 32767;
     } else if val < -32768 {
         return -32768;
     }
     return val;
+}
+
+
+fn add_u2_to_u8_vec(add: &mut Vec<u8>, with: [u8; 2]) {
+    for item in with {
+        add.push(item)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,49 @@ use game_data_resolver::visualizer;
 use game_data_resolver::reader;
 use game_data_resolver::parser;
 use game_data_resolver::decoder;
+use std::f32::consts::PI;
+use std::i16;
+use hound;
 
-static FILE_PATH: &str = "./line.scd";
+static FILE_PATH: &str = "./2.scd";
 
 fn main() {
     let mut buffer = reader::Buffer::from_file(FILE_PATH);
     let metadata = parser::sqex_scd::Metadata::new(&mut buffer);
-    let decoded = decoder::sqex_scd::decode(metadata, &mut buffer);
-    println!("{}", decoded.len());
-    for (index, byte) in decoded.iter().enumerate() {
-        if index % 16 == 0 {
-            print!("\x1b[39;49m\n")
-        }
-        if *byte > 0 {
-            print!("\x1b[31;49m{:02x} ", byte)
-        } else {
-            print!("\x1b[39;49m{:02x} ", byte)
-        }
+    if metadata.entry_channels > 2 || metadata.entry_codex != 12 || metadata.entry_wave_format_ex != 7 {
+        panic!("Unsupported format");
     }
+    let decoded = decoder::sqex_scd::decode(&metadata, &mut buffer);
 
-    std::fs::write("./audio", &decoded).unwrap();
+    let spec = hound::WavSpec {
+        channels: metadata.entry_channels as u16,
+        sample_rate: metadata.entry_sample_rate as u32,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
+    for index in (0..decoded.len()).step_by(2) {
+        writer.write_sample(i16::from_le_bytes([decoded[index], decoded[index + 1]])).unwrap();
+    }
+    // for t in (0 .. 44100).map(|x| x as f32 / 44100.0) {
+    //     let sample = (t * 440.0 * 2.0 * PI).sin();
+    //     let amplitude = i16::MAX as f32;
+    //     writer.write_sample((sample * amplitude) as i16).unwrap();
+    // }
+
+    // println!("{}", decoded.len());
+    // for (index, byte) in decoded.iter().enumerate() {
+    //     if index % 16 == 0 {
+    //         print!("\x1b[39;49m\n")
+    //     }
+    //     if *byte > 0 {
+    //         print!("\x1b[31;49m{:02x} ", byte)
+    //     } else {
+    //         print!("\x1b[39;49m{:02x} ", byte)
+    //     }
+    // }
+    //
+    // std::fs::write("./audio", &decoded).unwrap();
 
     //visualizer::buffer::print(&buffer);
 }
